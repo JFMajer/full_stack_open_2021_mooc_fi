4151a78f21847395d17019b9076f78783de9aa2c
JFMajer <jakubmajer@live.com>
2021-08-10 20:34:16 +0200

    4.13 and 4.14 without tests

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -2,22 +2,52 @@ const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
-    response.json(blogs)
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
+
+blogRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
+})
+
+blogRouter.post('/', async (request, response) => {
+  const body = request.body
+  //console.log(body)
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0
   })
-  
-  blogRouter.post('/', async (request, response) => {
-    const body = request.body
-    //console.log(body)
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes || 0
-    })
-    //console.log(request.body)
-    const savedBlog = await blog.save()
-    response.json(savedBlog)
-  })
+  //console.log(request.body)
+  const savedBlog = await blog.save()
+  response.json(savedBlog)
+})
+
+blogRouter.put('/:id', async (request, response) => {
+  const { id } = request.params
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    likes: body.likes
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(id, blog, { new: true, runValidators: true })
+  //console.log(updatedBlog)
+  response.json(updatedBlog)
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
 
 module.exports = blogRouter
